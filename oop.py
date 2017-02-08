bdc105a58a472364da6461dfc89ad5c45c76d8b6
kayuyumwaura <mwaurakayuyu@gmail.com>
2017-02-08 10:37:56 +0300

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,19 @@
+Class Customer(object):
+def __init__ (self, username, password)
+
+self.username = username
+self.password = password
+
+def login(self, username, password):
+	attempts = 0
+	while attempts <=2
+	input = input("Please enter your username and password")
+	if self.password == imput_password
+	print ("login successful. Welcome", username!)
+	return Home
+
+else:
+	print ("invalid username or password")
+	attempts >=3
+	print ("login failed. forgot password?")
+	return False
